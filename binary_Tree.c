075f30f6e32a548e22d75e17b27ec965a1dde805
mohit sharma <you@example.com>
2019-10-07 03:50:09 +0530

    time Complexity O(n)

---
+++ binary_Tree.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node
+{
+	int data;
+	struct node* left;
+	struct node* right;
+
+}node;
+node* head=NULL;
+node* p;
+node* root;
+node* a[104];
+int front=0;
+int rear=0;
+node* create_Node(int data)
+{
+	node* t=(node*)malloc(sizeof(node));
+	t->data=data;
+	t->left=NULL;
+	t->right=NULL;
+	return t;
+}
+void insert_Node(node* t)
+{
+	if(head==NULL)
+	{
+		head=t;
+		root=t;
+		en_queue(t);
+	}
+	else
+	{
+		if(root->left==NULL)
+		{
+			root->left=t;
+			en_queue(t);
+		}	
+		else if(root->left!=NULL && root->right==NULL)
+		{
+			root->right=t;
+			en_queue(t);
+			de_queue();
+		}
+		root=a[front];
+		
+		
+	}
+
+
+}
+void en_queue(node* add)
+{
+	
+	a[rear]=add;
+	rear++;
+}
+void de_queue()
+{
+	front++;
+}
+
+void main()
+{
+	int n;
+	int d;
+	printf("************************Binary Tree***************************");
+	printf("\n Enter no. of nodes in a binary Tree");
+	scanf("%d",&n);
+	printf("\n Enter data to Node");
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",&d);
+		node* t=create_Node(d);
+		insert_Node(t);
+
+	}
+	printf("\n %d",head->data);
+	printf("\n %d",head->left->data);
+	printf("\n %d",head->right->data);
+	printf("\n %d",head->left->left->data);
+
+
+}
